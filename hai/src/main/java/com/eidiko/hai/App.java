1a8498ccf00676f95935055f5ab83150683d67d3
harshav339 <harshav339@users.noreply.github.com>
2019-04-16 04:19:50 -0700

    first commit

---
+++ hai/src/main/java/com/eidiko/hai/App.java
@@ -0,0 +1,13 @@
+package com.eidiko.hai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
